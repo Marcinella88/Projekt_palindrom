c2cd674acbee356b3474833f1a4150711aa92132
Marcin Czapski <marcinczapski1988@gmail.com>
2025-03-06 18:21:12 +0100

    Final update

--- Palindrom.py
+++ Palindrom.py
@@ -1,9 +1,9 @@
 # Zadanie palindrom
 
-def czy_palindrom(word):
-    if word.lower() == word[::-1].lower():
-       print(f"Słowo: {word} to palindrom!")
-    else:
-       print(f"Słowo: {word} nie jest palindromem.")
+def czy_palindrom(word): 
+    return word.lower() == word[::-1].lower()
 
-czy_palindrom('Ania') # Tu wpisz słowo, aby sprawdzić czy to palindrom.
+if czy_palindrom('Anna'): 
+    print(f"Słowo to palindrom!")                # Tu wpisz słowo, aby sprawdzić czy to palindrom.        
+else:
+    print(f"Słowo to nie palindrom!")
