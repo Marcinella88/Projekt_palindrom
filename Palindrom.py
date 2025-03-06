0939c3552af16c3cd1b1e9eb0171a0cb16a79795
Marcin Czapski <marcinczapski1988@gmail.com>
2025-03-06 18:22:40 +0100

    Final update 2

--- Palindrom.py
+++ Palindrom.py
@@ -3,7 +3,7 @@
 def czy_palindrom(word): 
     return word.lower() == word[::-1].lower()
 
-if czy_palindrom('Anna'): 
-    print(f"Słowo to palindrom!")                # Tu wpisz słowo, aby sprawdzić czy to palindrom.        
+if czy_palindrom('Anna'):                        # Tu wpisz słowo, aby sprawdzić czy to palindrom. 
+    print(f"Słowo to palindrom!")       
 else:
     print(f"Słowo to nie palindrom!")
